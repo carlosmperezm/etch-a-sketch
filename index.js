990a9c04598ecddff13b5c380ec5b3713be65dd2
CarlosPerez <carlosmperez2901@gmail.com>
2024-12-30 18:14:21 -0800

    Fix the board background
    The hover over the board no longer changes the board's background color;

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const boardSection = document.querySelector(".board");
 const eraserButton = document.querySelector("#eraser");
 const clearButton = document.querySelector("#clear");
 
+const boardSectionHeight = window.getComputedStyle(boardSection).getPropertyValue("height");
+const boardSectionHeightInt = parseInt(boardSectionHeight.slice(0, -2));
+
+
 let color;
 
 function generateGrid(numberOfSquares) {
-  const dimesions = numberOfSquares + numberOfSquares
+  const dimesions = numberOfSquares * numberOfSquares
 
-  for (let i = 0; i < dimesions; i++) {
+  for (let i = 0; i < boardSectionHeightInt; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
+    div.style.height = `${boardSectionHeightInt / numberOfSquares}px`;
+    div.style.width = `${boardSectionHeightInt / numberOfSquares}px`;
     boardSection.appendChild(div);
   }
 }
@@ -17,6 +23,7 @@ function generateGrid(numberOfSquares) {
 function changeBackgroundColor(domElement, color) {
 
   domElement.style.backgroundColor = color || `rgb(${getRandomRgb()}, ${getRandomRgb()}, ${getRandomRgb()})`;
+  console.log(parseInt(boardSectionHeight.slice(0, -2)));
 }
 
 
@@ -24,7 +31,10 @@ function getRandomRgb() {
   return Math.floor(Math.random() * 255) + 1;
 }
 
-generateGrid(64);
+generateGrid(16);
 
 eraserButton.addEventListener("click", () => color = "White")
-boardSection.addEventListener("mouseover", evnt => changeBackgroundColor(evnt.target, color));
+boardSection.addEventListener("mouseover", evnt => {
+  if (evnt.target.className != "board") changeBackgroundColor(evnt.target, color);
+});
+
