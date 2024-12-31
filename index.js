b8c131458df2f63f3fa4b3f2ca7ffbfa5762cd03
CarlosPerez <carlosmperez2901@gmail.com>
2024-12-30 17:20:53 -0800

    Add functionality to the squares to change the color
    Squares changes their background color when a mouse overs over them.

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const boardSection = document.querySelector(".board");
 
 function generateGrid(numberOfSquares) {
-  for (let i = 0; i < numberOfSquares; i++) {
+  const dimesions = numberOfSquares + numberOfSquares
+
+  for (let i = 0; i < dimesions; i++) {
     let div = document.createElement("div");
     div.classList.add("square");
+    div.style.flexBasis = numberOfSquares;
     boardSection.appendChild(div);
   }
 }
 
-generateGrid(16);
+function changeBackgroundColor(domElement) {
+
+  domElement.style.backgroundColor = `rgb(${getRandomRgb()}, ${getRandomRgb()}, ${getRandomRgb()})`;
+}
+
+
+function getRandomRgb() {
+  return Math.floor(Math.random() * 255) + 1;
+}
+
+boardSection.addEventListener("mouseover", (evnt) => changeBackgroundColor(evnt.target));
+
+
+
+
+generateGrid(1400);
 
